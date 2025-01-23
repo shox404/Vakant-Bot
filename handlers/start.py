5346146252fe1e4218be4e3396533db7652edcd2
M-216 <mardonbekakbarbekov@gmail.com>
2025-01-23 13:08:26 +0500

    Update

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,27 @@
+from aiogram import Router, types
 from aiogram.types import Message
 from aiogram.filters import CommandStart
-from aiogram import Router
+from datetime import datetime
+from data.config import ADMINS
+
 
 start_router = Router()
 
-
 @start_router.message(CommandStart())
-async def start(message: Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+async def add_user(message: Message):
+    
+    current_time = datetime.now()
+    user_id = message.from_user.id
+
+    
+    if str(user_id) in ADMINS:
+        
+        if current_time.hour < 12:
+            await message.answer(f"Good morning. Welcome to the bot ADMIN <b>{message.from_user.full_name}</b>! ")
+        elif current_time.hour < 18 and current_time>12:
+           await message.answer(f"Good afternoon. Welcome to the bot ADMIN <b>{message.from_user.full_name}</b>! ")
+        else:
+           await message.answer(f"Good evening. Welcome to the bot ADMIN <b>{message.from_user.full_name}</b>! ")
+    else:
+       
+        await message.answer(f"Hello, Welcome to the bot <b>{message.from_user.full_name}</b>!")
