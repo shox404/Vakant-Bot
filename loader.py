66c7ec56656c5bfdbe45ab1400c52429688b85cc
shox404 <thesh8080@gmail.com>
2025-01-28 10:17:22 +0500

    Update

--- loader.py
+++ loader.py
@@ -1,6 +1,5 @@
 import logging
 from aiogram import Bot, Dispatcher
-from aiogram.types import CallbackQuery
 from aiogram.fsm.storage.memory import MemoryStorage
 from data import config
 from aiogram.client.bot import DefaultBotProperties
